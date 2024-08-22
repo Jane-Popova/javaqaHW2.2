eb1e74eb6717e9cb7ed34ba9bcf0c3e2a1eb7289
Evgenia Popova <jane477@yandex.ru>
2024-08-22 08:33:40 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100; // начальный счёт
-        int y = 1100; // сумма пополнения
+        int startBalance = 100; // начальный счёт
+        int accountReplenishment = 1100; // сумма пополнения
         int bonus; // Количество бонусов
 
-        if (y >= 1000) {
-            bonus = y/100;
+        if (accountReplenishment > 1000) {
+            bonus = accountReplenishment / 100;
         } else {
             bonus = 0;
         }
 
-        int z = x + y + bonus; // Итоговый счёт
+        int finalBalance = startBalance + accountReplenishment + bonus; // Итоговый счёт
 
         System.out.println("Количество бонусов: " + bonus + " рублей");
-        System.out.println("Итоговый счёт: " + z + " рублей");
+        System.out.println("Итоговый счёт: " + finalBalance + " рублей");
     }
 }
